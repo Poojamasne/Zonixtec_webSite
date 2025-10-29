5adf2431a93ba30c0e4e8b570d148ecd0a0a0d50
Aniket Sangale <aniketsangale23@gmail.com>
2025-10-29 14:47:11 +0530

    Update carrer.html

--- server/config/db.php
+++ server/config/db.php
@@ -2,11 +2,10 @@
 // config/db.php
 // Standard PDO connection with error handling and charset
 
-$host = "sql102.infinityfree.com";
-$dbname = "if0_40102304_yourdb"; // replace with your DB name
-$user = "if0_40102304";
-$pass = "root@123"; // MySQL password
-
+$host = 'localhost';
+$dbname = 'u627961759_zonixtec';
+$user = 'u627961759_root';
+$pass = 'Zonixtec@123';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
